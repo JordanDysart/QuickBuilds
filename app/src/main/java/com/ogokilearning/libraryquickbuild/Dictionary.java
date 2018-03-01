5757289089318f442a52dce601f382415403be8c
Jordan Dysart <dysartjordan@gmail.com>
2018-03-01 14:04:14 -0600

    "initial commit"

---
+++ app/src/main/java/com/ogokilearning/libraryquickbuild/Dictionary.java
@@ -0,0 +1,19 @@
+package com.ogokilearning.libraryquickbuild;
+
+/**
+ * Created by ogoki on 2018-03-01.
+ *
+ *
+ */
+
+public class Dictionary {
+
+
+    public static int[] makeEmptyIntArray(int size) {
+        return new int[size];
+    }
+
+    public static String[] makeEmptyStringArray(int size) {
+        return new String[size];
+    }
+}
